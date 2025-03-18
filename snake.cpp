cd1d31ce2cead1270a2112a0b00dbbd18c152fc0
D4rkN00dl3s <martin.osit.2057@gmail.com>
2025-03-18 23:55:21 +0200

    Added borders

--- snake.cpp
+++ snake.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <termios.h>   // For setting terminal attributes
 #include <csignal>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -34,6 +35,8 @@ void getTerminalSize(int &rows, int &cols) {
 struct termios original_termios;
 
 void restoreTerminalSettings() {
+    system("clear");
+    cout << "Terminating program" << endl;
     tcsetattr(STDIN_FILENO, TCSANOW, &original_termios);
 }
 
@@ -47,6 +50,11 @@ void enableRawMode() {
     atexit(restoreTerminalSettings);
 }
 
+void setNonBlockingInput() {
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+}
+
 void moveCursorTo(int row, int col) {
     cout << "\033[" << row << ";" << col << "H";
 }
@@ -54,7 +62,7 @@ void moveCursorTo(int row, int col) {
 // Global Variables
 int rows = 0, cols = 0;
 int fx = 0, fy = 0;
-char ch;
+char dir = 'd';
 bool run = true;
 deque<pair<int, int>> snake;
 
@@ -65,7 +73,35 @@ void CreateFoodPosition(int row, int col){
 
 void CreateFood (){
     moveCursorTo(fy, fx);
-    cout << "@" << endl;
+    cout << "@" << flush;
+}
+
+void DrawBorders(int row, int col){
+    int width = 60;
+    int height = 20;
+
+    int top = (row - height) / 2;  // Center vertically
+    int left = (col - width) / 2;  // Center horizontally
+
+    // Draw top border
+    moveCursorTo(top-1, left);
+    for (int i = 0; i < width; i++) {
+        cout << "_" << flush;
+    }
+
+    // Draw bottom border
+    moveCursorTo(top + height, left);
+    for (int i = 0; i < width; i++) {
+        cout << "_" << flush;
+    }
+
+    // Draw left and right borders
+    for (int i = 0; i <= height; i++) {
+        moveCursorTo(top + i, left);
+        cout << "|" << flush;
+        moveCursorTo(top + i, left + width);
+        cout << "|" << flush;
+    }
 }
 
 void DrawSnake() {
@@ -76,7 +112,17 @@ void DrawSnake() {
     cout.flush();
 }
 
-void UpdateSnake(int currRow, int currCol) {
+void UpdateSnake() {
+    int currRow = snake.front().first;
+    int currCol = snake.front().second;
+
+    switch (dir) {
+        case 'd': currCol++; break;
+        case 'a': currCol--; break;
+        case 'w': currRow--; break;
+        case 's': currRow++; break;
+    }
+
     // Add new head to the snake
     snake.push_front({currRow, currCol});
 
@@ -98,11 +144,15 @@ void UpdateSnake(int currRow, int currCol) {
 int main() {
     system("clear");
     enableRawMode();
+    setNonBlockingInput();
 
     getTerminalSize(rows, cols);
 
     int midRow = rows / 2;
     int midCol = cols / 2;
+
+    DrawBorders(rows, cols);
+
     snake.push_back({midRow, midCol});
 
     //Snake Starting Point
@@ -117,24 +167,21 @@ int main() {
 
     //Main Game Loop
     while (run) {
+        char ch;
         if (read(STDIN_FILENO, &ch, 1) > 0) {  // Read key press
-            int currRow = snake.front().first;
-            int currCol = snake.front().second;
-
             switch (ch) {
-                case 'd': currCol++; break;
-                case 'a': currCol--; break;
-                case 'w': currRow--; break;
-                case 's': currRow++; break;
+                case 'd': if(dir != 'a') dir = 'd'; break;
+                case 'a': if(dir != 'd') dir = 'a'; break;
+                case 'w': if(dir != 's') dir = 'w'; break;
+                case 's': if(dir != 'w') dir = 's'; break;
                 case 'q': run = false; break;
-                default: continue;
             }
-
-            UpdateSnake(currRow, currCol);
         }
+
+        UpdateSnake();
+
+        usleep(150000); // Delay for movement speed 150ms
     }
 
-    system("clear");
-    cout << "Terminating program" << endl;
     return 0;
 }
